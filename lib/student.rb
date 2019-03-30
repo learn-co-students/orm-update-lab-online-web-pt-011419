c4800692960f62e4b0cced7df2eeac85d24e2141
dylanclements <dclements@protonmail.com>
2019-03-30 07:34:29 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -6,7 +6,7 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id
 
-  def initialize(id = nil, name, grade, )
+  def initialize(id = nil, name, grade)
     @name = name
     @grade = grade
     @id = id
@@ -61,11 +61,10 @@ class Student
   end
 
   def self.new_from_db(row)
-    test = self.new(row[0], row[1], row[2])
-    binding.pry
+    self.new(row[0], row[1], row[2])
   end
   
-  def self.find_by_name
+  def self.find_by_name(name)
     sql = <<-SQL
     SELECT * FROM students
     WHERE name = ?
