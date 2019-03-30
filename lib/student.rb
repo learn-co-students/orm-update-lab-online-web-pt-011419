18dfac0996eb20aa745e1d9b66e70b4f8f28c5d2
Alyssa Brown <alyssa.brown@live.com>
2019-03-30 01:02:26 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -17,26 +17,51 @@ def self.create_table
         id INTEGER PRIMARY KEY,
         name TEXT,
         grade INTEGER
-        )
+        ) 
         SQL
     DB[:conn].execute(sql) 
   end 
   
   def self.drop_table 
-      sql = "DROP TABLE IF EXISTS students"
-    
+      sql =  "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end 
   
   def save 
+    if self.id
+    self.update
+  else
    sql =  <<-SQL
       INSERT INTO students (name,grade) VALUES (?,?)
-      INSERT INTO students (name,grade) VALUES (?,?)
         SQL
-        binding.pry 
-     DB[:conn].exectue(sql,self.name,self.grade)
+     DB[:conn].execute(sql,self.name,self.grade)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end 
+end 
+  
+  def update 
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
+    
+  def self.create(name,grade)
+    student = Student.new(name, grade)
+    student.save
+    student
+  end
+
+  
+  def self.new_from_db(row)
+   id = row[0]
+   name = row[1]
+   grade = row[2]
+   self.new(id, name, grade)
+ end
+ 
+ def self.find_by_name(name)
+    sql = "SELECT * FROM students WHERE name = ?"
+    DB[:conn].execute(sql, name).map { |row| new_from_db(row) }.first
+  end
    
 end 
 
